a4ca8e058321d5a160850b94b1cf19e1575cf554
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2019-01-29 18:07:27 +0100

    speakers section wip

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,16 +6,26 @@ window.onload = function() {
     });
 
 
-    document.getElementById("j_tickets").onclick = function() {
-        var modal = document.getElementById("j_modal");
-        modal.style.display = 'block';
-    };
+    // document.getElementById("j_tickets").onclick = function() {
+    //     var modal = document.getElementById("j_modal");
+    //     modal.style.display = 'block';
+    // };
 
 
     document.getElementById("j_modal_close").onclick = function() {
         var modal = document.getElementById("j_modal");
         modal.style.display = 'none';
+        document.getElementById("j_modal_title").textContent = '';
     };
+
+
+    document.querySelector('.j_speaker').addEventListener('click', function(event) {
+        var data = event.target.dataset;
+        var modal = document.getElementById("j_modal");
+        modal.style.display = 'block';
+        document.getElementById("j_modal_title").textContent = data.title;
+    });
+
 
 
 
